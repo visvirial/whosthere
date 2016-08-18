301bf8c8e2758b5e6274ad4d8778d79e5a83fd99
Vis Virial <visvirial@gmail.com>
2016-08-18 11:56:44 +0900

    printing connected node list on activity.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var async = require('async');
 var exec = require('child_process').exec;
 var Slack = require('slack-node');
 var moment = require('moment');
+var sprintf = require('sprintf-js').sprintf;
 
 var config = require('./config.json');
 
@@ -69,14 +70,12 @@ var list_nodes = function(cb) {
 var slack_notify = function(node, type) {
 	var message = node.label + ' ' + (type=='connect' ? 'が接続しました' : 'が切断しました');
 	console.log('[' + new Date().toString() + '] ' + message);
-	var text = '';
-	text += moment().format('YYYY/MM/DD HH:mm:ss.SS');
-	text += '\n';
-	text += node.hostname;
-	text += ' / ';
-	text += '<http://' + node.ip + '/|' + node.ip + '>';
-	text += ' / ';
-	text += node.mac;
+	var text = sprintf(
+		'%1$s\n%2$s / <http://%3$s/|%3$s> / %4$s',
+		moment().format('YYYY/MM/DD HH:mm:ss.SS'),
+		node.hostname,
+		node.ip,
+		node.mac);
 	slack.webhook({
 		channel: config.slack.channel,
 		username: node.user.label,
@@ -95,14 +94,45 @@ var slack_notify = function(node, type) {
 var current_nodes = {};
 
 /**
+ * Send connected node list to Slack.
+ */
+var slack_list = function() {
+	var nodes = [];
+	for(var n in current_nodes) {
+		nodes.push(current_nodes[n]);
+	}
+	nodes.sort(function(a, b) {
+		return a.owner.localeCompare(b.owner);
+	});
+	var text = '';
+	nodes.forEach(function(n) {
+		text += sprintf('[%s] %s\n', n.user.label, n.label);
+	});
+	slack.webhook({
+		channel: config.slack.channel,
+		username: 'whosthere',
+		icon_emoji: ':grey_question:',
+		attachments: [{
+			fallback: text,
+			color: 'grey',
+			pretext: '接続一覧',
+			text: text,
+		}],
+	}, function(err, res) {
+	});
+};
+
+/**
  * Check current nodes repeatedly, and print the results.
  */
 var check_nodes = function() {
 	list_nodes(function(nodes) {
+		var changed = false;
 		// Explorer currently connected nodes.
 		for(var mac in nodes) {
 			if(current_nodes[mac] == undefined) {
 				slack_notify(nodes[mac], 'connect');
+				changed = true;
 			}
 			current_nodes[mac] = nodes[mac];
 			current_nodes[mac].last_seen = new Date().getTime();
@@ -111,9 +141,11 @@ var check_nodes = function() {
 		for(var mac in current_nodes) {
 			if(new Date().getTime() - current_nodes[mac].last_seen > config.threshold_time) {
 				slack_notify(current_nodes[mac], 'disconnect');
+				changed = true;
 				delete current_nodes[mac];
 			}
 		}
+		if(changed) slack_list();
 		setTimeout(check_nodes, config.serach_interval);
 	});
 };
